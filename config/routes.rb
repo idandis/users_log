792119e5973988a93fc861861bc1d222ac751973
Iana Longo Dandis <77735163+idandis@users.noreply.github.com>
2023-01-29 15:58:25 +0100

    aggiunta la pagina dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'main#home'
+  
+  get 'home/dashboard', to: 'main#dashboard'  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
