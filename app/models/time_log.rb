338dbaf63cc09c25ad94d874da3538c381cf08b5
Iana Longo Dandis <77735163+idandis@users.noreply.github.com>
2023-01-30 08:38:43 +0100

    TimeLog + time_spent

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,3 +1,9 @@
 class TimeLog < ApplicationRecord
   belongs_to :user
+
+  validates :summary, presence: true
+  validates :time_spent, presence: true
+  validates :log_date, presence: true
+  
+
 end
