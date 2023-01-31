cd327d34b5c0c1fd05b0402c8ad675ea028c045e
Iana Longo Dandis <77735163+idandis@users.noreply.github.com>
2023-01-31 17:23:29 +0100

    create db with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+2.times do 
+    Article.create(title: "this title is generate by seed", body:"the body of this article is generate by seed")
+end
